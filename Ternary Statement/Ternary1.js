da96525b0509244d7b82cdcc7d5cdd20d4d4cae6
Mehedi Hasan <mehedih.dev@gmail.com>
2023-03-31 17:34:02 +0600

    changes in ternary operator

--- Ternary Statement/Ternary1.js
+++ Ternary Statement/Ternary1.js
@@ -1,9 +1,13 @@
-const num= 0
-// if (num>1){
-//     console.log(`The number is greater then 1`)
-// } else if(num===2){
-//     console.log(`The is identical to 2`)
-// } else{
-//     console.log(`The number is less then 1`)
-// }
-num>1 ? console.log('The number is greater then 1') : console.log('The number is unmatched')
+// const num= 0
+// // if (num>1){
+// //     console.log(`The number is greater then 1`)
+// // } else if(num===2){
+// //     console.log(`The is identical to 2`)
+// // } else{
+// //     console.log(`The number is less then 1`)
+// // }
+// num>1 ? console.log('The number is greater then 1') : console.log('The number is unmatched')
+// Here is another example by using ternary operator:
+const value= 1
+const result= value>1 ? 'The number is greater then 1' : 'The number is unmatched'
+console.log(result)
