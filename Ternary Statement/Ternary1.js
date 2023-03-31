d96c4d11e4aac9de5c9179f5b941b18a9eefae26
Mehedi Hasan <mehedih.dev@gmail.com>
2023-03-31 17:27:06 +0600

    added new files

---
+++ Ternary Statement/Ternary1.js
@@ -0,0 +1,9 @@
+const num= 0
+// if (num>1){
+//     console.log(`The number is greater then 1`)
+// } else if(num===2){
+//     console.log(`The is identical to 2`)
+// } else{
+//     console.log(`The number is less then 1`)
+// }
+num>1 ? console.log('The number is greater then 1') : console.log('The number is unmatched')
